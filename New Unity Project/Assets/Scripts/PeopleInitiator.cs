e4b56ede43cce9a01398a89e08d65e6322b541ec
onestarYX <40019226+onestarYX@users.noreply.github.com>
2020-04-22 14:02:49 -0700

    Uploaded prototype 1 built with Unity primitive models

---
+++ New Unity Project/Assets/Scripts/PeopleInitiator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleInitiator : MonoBehaviour
+{
+    public GameObject personPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numOfPeople = 300;
+        float xRange = 45;
+        float zRange = 45;
+        for (int i = 0; i < numOfPeople; i++)
+        {
+            float initPosX = Random.Range(-xRange, xRange);
+            float initPosZ = Random.Range(-zRange, zRange);
+            Vector3 initPos = new Vector3(initPosX, 0.5f, initPosZ);
+            Instantiate(personPrefab, initPos, personPrefab.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RandomMove.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    private int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateDirection", 2, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 10.0f;
+        switch(direction)
+        {
+            case 0:
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                break;
+            case 1:
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+                break;
+            case 2:
+                transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+                break;
+            case 3:
+                transform.Translate(Vector3.right * Time.deltaTime * -speed);
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    void UpdateDirection()
+    {
+        direction = Random.Range(0, 4);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch (direction)
+        {
+            case 0:
+                direction = 2;
+                break;
+            case 1:
+                direction = 3;
+                break;
+            case 2:
+                direction = 0;
+                break;
+            case 3:
+                direction = 1;
+                break;
+            default:
+                break;
+        }
+
+        if(other.gameObject.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+}
