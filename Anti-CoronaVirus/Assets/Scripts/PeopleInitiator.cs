b47264358d4c1180f8b971fdaafb6f6029a7a2a8
onestarYX <40019226+onestarYX@users.noreply.github.com>
2020-05-20 13:45:04 -0700

    Finished FindNearestRoad

--- Anti-CoronaVirus/Assets/Scripts/PeopleInitiator.cs
+++ Anti-CoronaVirus/Assets/Scripts/PeopleInitiator.cs
@@ -6,30 +6,17 @@ public class PeopleInitiator : MonoBehaviour
 {
     public GameObject personPrefab;
     public List<GameObject> people = new List<GameObject>();
-    public int pplPerHouse = 1;
+    public int pplPerHouse = 3;
+    private float personY = 0.5f;
+    private float spawnOffset = 3;
     private RoadManager roadManager;
+    public GameObject startRoad;
 
     // Start is called before the first frame update
     void Start()
     {
-        float spawnOffset = 3;
-
-        for (int i = 0; i < pplPerHouse; i++)
-        {
-            float initPosX = Random.Range(transform.position.x - spawnOffset, transform.position.x + spawnOffset);
-            float initPosZ = Random.Range(transform.position.z - spawnOffset, transform.position.z + spawnOffset);
-            Vector3 initPos = new Vector3(initPosX, 0.5f, initPosZ);
-            GameObject newPerson = Instantiate(personPrefab, initPos, personPrefab.transform.rotation);
-            people.Add(newPerson);
-        }
-
-        // For test purposes
         roadManager = GameObject.Find("Roads").GetComponent<RoadManager>();
-        GameObject startRoad = null;
-        AgentMove someAgentMove = people[0].GetComponent<AgentMove>();
-        someAgentMove.Setup(0, 0, transform.position.x, transform.position.z, 5);
-        StartCoroutine(waiter(startRoad, someAgentMove));
-
+        StartCoroutine(waiter());
     }
 
     // Update is called once per frame
@@ -38,11 +25,66 @@ public class PeopleInitiator : MonoBehaviour
         
     }
 
-    IEnumerator waiter(GameObject startRoad, AgentMove someAgentMove)
+    IEnumerator waiter()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(3);
         startRoad = findNearestStartRoad(transform.position);
-        someAgentMove.SetOnRoad(startRoad, startRoad.transform.position + new Vector3(0, 0, 45));
+        List<AgentMove> agentMoveScriptsList = new List<AgentMove>();
+
+        for (int i = 0; i < pplPerHouse; i++)
+        {
+            float initPosX = Random.Range(transform.position.x - spawnOffset, transform.position.x + spawnOffset);
+            float initPosZ = Random.Range(transform.position.z - spawnOffset, transform.position.z + spawnOffset);
+            Vector3 initPos = new Vector3(initPosX, 0.5f, initPosZ);
+            GameObject newPerson = Instantiate(personPrefab, initPos, personPrefab.transform.rotation);
+            people.Add(newPerson);
+            AgentMove agentMoveScript = newPerson.GetComponent<AgentMove>();
+            agentMoveScriptsList.Add(agentMoveScript);
+        }
+
+        for (int i = 0; i < pplPerHouse; i++)
+        {
+            agentMoveScriptsList[i].Setup(0, 0, transform.position.x, transform.position.z, 5);
+        }
+
+        for (int i = 0; i < pplPerHouse; i++)
+        {
+            yield return new WaitForSeconds(3);
+            agentMoveScriptsList[i].SetOnRoad(startRoad, CalculateStartPos(transform.position, startRoad));
+        }
+    }
+
+    Vector3 CalculateStartPos(Vector3 housePos, GameObject startRoad)
+    {
+        if (startRoad.transform.rotation.y == 90)
+        {
+            if (housePos.x < (startRoad.transform.position.x - startRoad.GetComponent<Collider>().bounds.size.z / 2 + startRoad.GetComponent<Collider>().bounds.size.x / 2))
+            {
+                float startPosX = startRoad.transform.position.x - startRoad.GetComponent<Collider>().bounds.size.z / 2 + startRoad.GetComponent<Collider>().bounds.size.x / 2;
+                return new Vector3(startPosX, personY, startRoad.transform.position.z);
+            } else if (housePos.x > (startRoad.transform.position.x + startRoad.GetComponent<Collider>().bounds.size.z / 2 - startRoad.GetComponent<Collider>().bounds.size.x / 2))
+            {
+                float startPosX = startRoad.transform.position.x + startRoad.GetComponent<Collider>().bounds.size.z / 2 - startRoad.GetComponent<Collider>().bounds.size.x / 2;
+                return new Vector3(startPosX, personY, startRoad.transform.position.z);
+            } else
+            {
+                return new Vector3(housePos.x, personY, startRoad.transform.position.z);
+            }
+        } else
+        {
+            if (housePos.z < (startRoad.transform.position.z - startRoad.GetComponent<Collider>().bounds.size.z / 2 + startRoad.GetComponent<Collider>().bounds.size.x / 2))
+            {
+                float startPosZ = startRoad.transform.position.z - startRoad.GetComponent<Collider>().bounds.size.z / 2 + startRoad.GetComponent<Collider>().bounds.size.x / 2;
+                return new Vector3(startRoad.transform.position.x, personY, startPosZ);
+            } else if (housePos.z > (startRoad.transform.position.z + startRoad.GetComponent<Collider>().bounds.size.z / 2 - startRoad.GetComponent<Collider>().bounds.size.x / 2))
+            {
+                float startPosZ = startRoad.transform.position.z + startRoad.GetComponent<Collider>().bounds.size.z / 2 - startRoad.GetComponent<Collider>().bounds.size.x / 2;
+                return new Vector3(startRoad.transform.position.x, personY, startPosZ);
+            } else
+            {
+                return new Vector3(startRoad.transform.position.x, personY, housePos.z);
+            }
+        }
     }
 
     GameObject findNearestStartRoad(Vector3 pos)
@@ -53,7 +95,33 @@ public class PeopleInitiator : MonoBehaviour
             Debug.LogError("error in fetching roads");
         }
 
-        return roadManager.roads[0];
+        float minDist = 10000;
+        float dist = -1;
+        GameObject roadToReturn = null;
+
+        foreach (GameObject road in roadManager.roads)
+        {
+            if (road.transform.rotation.y == 90)
+            {
+                dist = Mathf.Abs(pos.z - road.transform.position.z);
+            }
+            else
+            {
+                dist = Mathf.Abs(pos.x - road.transform.position.x);
+            }
+
+            if (dist < minDist)
+            {
+                minDist = dist;
+                roadToReturn = road;
+            }
+        }
+
+        if (dist == -1)
+        {
+            Debug.LogError("error in finding min dist road");
+        }
+        return roadToReturn;
     }
 
 }
