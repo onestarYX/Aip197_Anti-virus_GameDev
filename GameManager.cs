be7de4cec1982f50d864bdd94820264d5c752a25
Hainan Xiong <mittyhainan@gmail.com>
2020-11-18 00:15:44 -0800

    add some modifications without live update

--- GameManager.cs
+++ GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     //private StreamWriter writePtr;
     //private FileStream StreamPtr;
     //1
+    //public dataVisual dataVisualizer = new dataVisual();
 
     public TextMeshProUGUI dayText;
     public TextMeshProUGUI timeText;
@@ -25,23 +26,22 @@ public class GameManager : MonoBehaviour
     private int day;
     private string minStr;
     private string hourStr;
-    private string line = "Population,Infected,Detected,Recovered,Death Toll,Time Elapsed,Infected Rate,Recovered Rate\n";
+    private string line = "Population,Infected,Detected,Recovered,Death Toll,Time Elapsed,Infected Rate,Recovered Rate,Death Rate\n";
 
-    private int infected_rate;
-    private int detected_rate;
-    private int deaht_rate;
-
-    private int recover_rate;
-    private int population;
-    private int infected;
-    private int detected;
-    private int recovered;
-    private int death;
-    private int previousPopulation;
-    private int previousInfected;
-    private int previousDetected;
-    private int previousRecovered;
-    private int previousDeath;
+    private int infected_rate = 0;
+    private int detected_rate = 0;
+    private int death_rate = 0;
+    private int recover_rate = 0;
+    private int population = 0;
+    private int infected = 0;
+    private int detected = 0;
+    private int recovered = 0;
+    private int death = 0;
+    private int previousPopulation = 0;
+    private int previousInfected = 0;
+    private int previousDetected = 0;
+    private int previousRecovered = 0;
+    private int previousDeath = 0;
     private string path = @"/Users/mittyhainan/Desktop/Anti-CoronaVirus/Assets/Scripts/temp.csv";
 
     // Start is called before the first frame update
@@ -49,6 +49,7 @@ public class GameManager : MonoBehaviour
     {
         timePassed = 0;
         minute = 0;
+        day = 0;
         hour = 6;
         minStr = "00";
         hourStr = "06";
@@ -56,6 +57,7 @@ public class GameManager : MonoBehaviour
         houseManager = GameObject.Find("Residence").GetComponent<HouseManager>();
         //writePtr = new StreamWriter(path);
         //StreamPtr = new FileStream(path,FileMode.Open,FileAccess.ReadWrite, FileShare.ReadWrite);
+        //dataVisualizer.Awake();
     }
 
     // Update is called once per frame
@@ -66,7 +68,7 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-        timePassed += 5;
+        timePassed += 50;
         if(timePassed % 60 == 0)
         {
             minute++;
@@ -115,16 +117,34 @@ public class GameManager : MonoBehaviour
         recoveredText.text = "Recovered: " + recovered;
         deathText.text = "Deaths: " + death;
 
+        /*
+        dataVisualizer.addInfection(infected,GetCurTime());
+        dataVisualizer.addInfecti(detected,GetCurTime());
+        dataVisualizer.addInfection(recovered,GetCurTime());
+        dataVisualizer.addInfection(death,GetCurTime());
+        */
+
         if(previousPopulation != population || previousInfected != infected || previousDetected != detected || previousRecovered != recovered || previousDeath != death){
             if(previousInfected != infected){
                 infected_rate = GetCurTime();
+                //dataVisualizer.addInfection(infected,infected_rate);
             }
             if(previousRecovered != recovered){
                 recover_rate = GetCurTime();
+                //dataVisualizer.addRecovered(recovered,recover_rate);
             }
+            if(previousDetected != detected){
+                detected_rate = GetCurTime();
+                //dataVisualizer.addRecovered(detected,detected_rate);
+            }
+            if(previousDeath != death){
+                death_rate = GetCurTime();
+                //dataVisualizer.addRecovered(death,death_rate);
+            }
+            
             using(StreamWriter writePtr = new StreamWriter(path)){
-                line = line + string.Format("{0},{1},{2},{3},{4},{5},{6},{7}\n", population,
-                    infected,detected,recovered,death,GetCurTime(),infected_rate,recover_rate);
+                line = line + string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}\n", population,
+                    infected,detected,recovered,death,GetCurTime(),infected_rate,recover_rate,death_rate);
                 writePtr.Write(line);
             }
             previousPopulation = population;
@@ -132,6 +152,7 @@ public class GameManager : MonoBehaviour
             previousDetected = detected;
             previousRecovered = recovered;
             previousDeath = death;
+            //dataVisualizer.presentGraph();
         }
     }
 
--- dataVisual.cs
+++ dataVisual.cs
@@ -5,22 +5,55 @@ using UnityEngine;
 public class dataVisual: MonoBehaviour
 {
     GraphMaster.UGuiGraph graph;
-    GraphMaster.UGuiDataSeriesXy series1;
+    GraphMaster.UGuiDataSeriesXy seriesInfected;
+    GraphMaster.UGuiDataSeriesXy seriesDetected;
+    GraphMaster.UGuiDataSeriesXy seriesRecovered;
+    GraphMaster.UGuiDataSeriesXy seriesDeath;
+    List<Vector2> infected = new List<Vector2>();
+    List<Vector2> detected = new List<Vector2>();
+    List<Vector2> recovered = new List<Vector2>();
+    List<Vector2> death = new List<Vector2>();
 
-    void Awake(){
-        graph = gameObject.GetComponent<GraphMaster.UGuiGraph>();
+    public void Awake(){
+        this.graph = gameObject.GetComponent<GraphMaster.UGuiGraph>();
 
-        graph.setRanges(0,60,0,3000);
+        this.graph.setRanges(0,30,0,50000);
 
-        List<Vector2> data = new List<Vector2>();
-        data.Add(new Vector2(0,0));
-        data.Add(new Vector2(10,100));
-        data.Add(new Vector2(20,350));
-        data.Add(new Vector2(40,600));
-        data.Add(new Vector2(50,2900));
+        infected.Add(new Vector2(0,0));
+        detected.Add(new Vector2(0,0));
+        recovered.Add(new Vector2(0,0));
+        death.Add(new Vector2(0,0));
 
+        this.seriesInfected = this.graph.addDataSeries<GraphMaster.UGuiDataSeriesXy>("infected",Color.yellow);
+        this.seriesDetected = this.graph.addDataSeries<GraphMaster.UGuiDataSeriesXy>("detected",Color.red);
+        this.seriesRecovered = this.graph.addDataSeries<GraphMaster.UGuiDataSeriesXy>("recovered",Color.blue);
+        this.seriesDeath = this.graph.addDataSeries<GraphMaster.UGuiDataSeriesXy>("death",Color.black);
+        this.seriesInfected.Data = infected;
+        this.seriesDetected.Data = detected;
+        this.seriesRecovered.Data = recovered;
+        this.seriesDeath.Data = death;
+    }
 
+    public void addInfection(int a, int b){
+        infected.Add(new Vector2(a,b));
+    }
 
-        series1 = graph.addDataSeries<GraphMaster.UGuiDataSeriesXy>("try",Color.red);
+    public void addDetected(int a, int b){
+        detected.Add(new Vector2(a,b));
+    }
+
+    public void addRecovered(int a, int b){
+        recovered.Add(new Vector2(a,b));
+    }
+
+    public void addDeath(int a, int b){
+        death.Add(new Vector2(a,b));
+    }
+
+    public void presentGraph(){
+        this.seriesInfected.Data = infected;
+        this.seriesDetected.Data = detected;
+        this.seriesRecovered.Data = recovered;
+        this.seriesDeath.Data = death;
     }
 }
